0671ad2b2638309c9fce91f1072bbe423bdbbfff
Amrit <amt.tmg@gmail.com>
2018-10-02 19:05:16 +0545

    SWQA-39 Test manager Create Test Suite
    -[x] Create Test Suite form

--- src/containers/App/Routes/TestManagerRoutes.jsx
+++ src/containers/App/Routes/TestManagerRoutes.jsx
@@ -2,7 +2,11 @@ import asyncComponent from "../../../helpers/AsyncFunc";
 
 export default [
   {
-    path: "test-manager/suite",
+    path: "test-manager/suite/list",
     component: asyncComponent(() => import("../../Admin/TestManager/TestSuite/List"))
+  },
+  {
+    path: "test-manager/suite/create/:companyId?/:teamId?",
+    component: asyncComponent(() => import("../../Admin/TestManager/TestSuite/Create"))
   },
 ]
