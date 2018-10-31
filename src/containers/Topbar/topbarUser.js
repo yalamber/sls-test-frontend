64cd38948290c9b3b1df94e5691c4c8c44fdbcf6
Amrit Tamang <amt.tmg@gmail.com>
2018-11-01 05:31:30 +0545

    SWQA-118 Bug Fixing

--- src/containers/Topbar/topbarUser.js
+++ src/containers/Topbar/topbarUser.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import Popover from '../../components/uielements/popover';
 import IntlMessages from '../../components/utility/intlMessages';
-import userpic from '../../image/user1.png';
 import authAction from '../../redux/auth/actions';
 import appAction from '../../redux/app/actions';
 import TopbarDropdownWrapper from './topbarDropdown.style';
 
-const { logout } = authAction;
-const { closeAll } = appAction;
+const {logout} = authAction;
+const {closeAll} = appAction;
+
 class TopbarUser extends Component {
   constructor(props) {
     super(props);
@@ -18,24 +18,20 @@ class TopbarUser extends Component {
       visible: false
     };
   }
+
   hide() {
-    this.setState({ visible: false });
+    this.setState({visible: false});
   }
+
   handleVisibleChange() {
-    this.setState({ visible: !this.state.visible });
+    this.setState({visible: !this.state.visible});
   }
 
   render() {
     const content = (
       <TopbarDropdownWrapper className="isoUserDropdown">
         <a className="isoDropdownLink">
-          <IntlMessages id="themeSwitcher.settings" />
-        </a>
-        <a className="isoDropdownLink">
-          <IntlMessages id="sidebar.feedback" />
-        </a>
-        <a className="isoDropdownLink">
-          <IntlMessages id="topbar.help" />
+          <IntlMessages id="themeSwitcher.settings"/>
         </a>
         <a
           className="isoDropdownLink"
@@ -44,7 +40,7 @@ class TopbarUser extends Component {
             this.props.closeAll();
           }}
         >
-          <IntlMessages id="topbar.logout" />
+          <IntlMessages id="topbar.logout"/>
         </a>
       </TopbarDropdownWrapper>
     );
@@ -58,12 +54,10 @@ class TopbarUser extends Component {
         arrowPointAtCenter={true}
         placement="bottomLeft"
       >
-        <div className="isoImgWrapper">
-          <img alt="user" src={userpic} />
-          <span className="userActivity online" />
-        </div>
+        <h3>Welcome Admin</h3>
       </Popover>
     );
   }
 }
-export default connect(null, { logout, closeAll })(TopbarUser);
+
+export default connect(null, {logout, closeAll})(TopbarUser);
