d6f36e79df754750028ad270b21759235de80cf4
rajivstha <stha.rajiv@gmail.com>
2018-11-04 20:53:45 +0545

    update to SWQA-136 task

--- src/Validations/usersValidation.js
+++ src/Validations/usersValidation.js
@@ -2,7 +2,10 @@ import {required, email} from './rules'
 
 export const userValidation = {
   status: [required],
-  username: [required],
+  username: [
+    {required: true, message: "This field is required."},
+    {whitespace: true, message: "Username must not have whitespace."}
+  ],
   email: [email, required],
   client: [required],
   company: [required],
