0ac6cfd412a3658b338ed1090e8cad96bc88d037
ty <tantyrohunter@gmail.com>
2018-11-20 13:11:38 +0800

    Init

---
+++ src/containers/Modules/Companies/teams/UserCreateEdit.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import { Row, Col, Spin } from "antd";
+import { withRouter } from "react-router-dom";
+import LayoutWrapper from "../../../../components/utility/layoutWrapper.js";
+import PageHeader from "../../../../components/utility/pageHeader";
+import basicStyle from "../../../../settings/basicStyle";
+import { getErrorDataFromApiResponseError } from "../../../../util/response-message";
+import { TitleWrapper, ComponentTitle } from "../../crud.style";
+import _ from "lodash";
+import Box from "../../../../components/utility/box";
+import UserForm from "../users/partials/UserForm";
+import { message } from "antd/lib/index";
+import {
+  getCompanyTeam,
+  addUser,
+  addUserToCompany
+} from "../../../../helpers/http-api-client";
+
+class Create extends Component {
+  constructor() {
+    super();
+    this.state = {
+      company: {},
+      team: {},
+      errors: {
+        details: []
+      },
+      loading: false
+    };
+    this.fetchData = this.fetchData.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    this.setState({ loading: true });
+    const { teamId } = this.props.match.params;
+    try {
+      let response = await getCompanyTeam(teamId);
+      this.setState({
+        loading: false,
+        team: response.data,
+        company: response.data.client
+      });
+    } catch(e) {
+      message.error("Problem occured.");
+      this.setState({ loading: false });
+    }
+  }
+
+  async handleSubmit(formData, resetForm) {
+    try {
+      this.setState({ loading: true });
+      let role = formData.role;
+      formData = _.omit(formData, "role");
+      let user = await addUser({ ...formData });
+      let companyUser = await addUserToCompany(user.data.userId, this.state.company.clientId, {
+        roleId: role,
+        status: formData.status
+      })
+      if (companyUser.status === 200) {
+        message.success("Successfully Saved");
+        resetForm();
+        this.setState({ errors: { details: [] } });
+        this.props.history.goBack();
+      }
+    } catch(error) {
+      this.setState({ errors: getErrorDataFromApiResponseError(error) });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  render() {
+    const { rowStyle, colStyle, gutter } = basicStyle;
+    return (
+      <LayoutWrapper>
+        <PageHeader>
+          {this.state.company.name} -> {this.state.team.name}
+        </PageHeader>
+        <Row style={rowStyle} gutter={gutter} justify="start">
+          <Col md={24} sm={24} xs={24} style={colStyle}>
+            <Box>
+              <TitleWrapper>
+                <ComponentTitle>Create Team Member</ComponentTitle>
+              </TitleWrapper>
+              <Spin spinning={this.state.loading}>
+                <UserForm
+                  relId={this.state.company.clientId}
+                  userType="clientUser"
+                  submit={this.handleSubmit}
+                  errors={this.state.errors}
+                />
+              </Spin>
+            </Box>
+          </Col>
+        </Row>
+      </LayoutWrapper>
+    );
+  }
+}
+
+export default withRouter(Create);
