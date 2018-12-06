3bc48bb72b62b448830c9c785cdff528b1f694c4
Yalamber Subba <yalamber@mundhum.com>
2018-12-06 13:58:10 +0545

    fixes

---
+++ src/validations/rules.js
@@ -0,0 +1,8 @@
+export const required = {
+  required: true,
+  message: 'This field is required.',
+};
+export const email = {
+  type: 'email',
+  message: 'Please enter valid email address.',
+};
