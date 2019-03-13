de096e80503c220f6d9147ac343f7c43cbeb25f9
Yalamber Subba <yalamber@mundhum.com>
2019-03-13 15:14:54 +0545

    Fixes to lang selector

--- src/containers/LanguageSwitcher/config.js
+++ src/containers/LanguageSwitcher/config.js
@@ -2,9 +2,7 @@ import { language } from "../../settings";
 
 import englishLang from "../../image/flag/uk.svg";
 import chineseLang from "../../image/flag/china.svg";
-import spanishLang from "../../image/flag/spain.svg";
 import frenchLang from "../../image/flag/france.svg";
-import italianLang from "../../image/flag/italy.svg";
 
 const config = {
   defaultLanguage: language,
@@ -22,23 +20,11 @@ const config = {
       icon: chineseLang
     },
     {
-      languageId: "spanish",
-      locale: "es",
-      text: "Spanish",
-      icon: spanishLang
-    },
-    {
       languageId: "french",
       locale: "fr",
       text: "French",
       icon: frenchLang
     },
-    {
-      languageId: "italian",
-      locale: "it",
-      text: "Italian",
-      icon: italianLang
-    }
   ]
 };
 
