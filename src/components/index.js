6e359a6ac26165a6058fa7f525f964634c8a6be4
Yalamber Subba <yalamber@mundhum.com>
2019-03-18 16:17:32 +0545

    remove unwanted components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-import notification from './notification';
 import ColorChoser from './colorChoser';
-import EditableComponent from './editableComponent';
+import notification from './notification';
 
-export { notification, ColorChoser, EditableComponent };
+export { ColorChoser, notification };
